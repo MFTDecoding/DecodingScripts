b079a05e5e096907a096b8a6137598b90bea250e
rsadek <arakotoz@cern.ch>
2021-03-10 21:35:00 +0100

    add more error cases to count

--- run_decoding_workflow.sh
+++ run_decoding_workflow.sh
@@ -6,7 +6,7 @@ if [ $# -ne ${NUMBER_OF_ARGS} ]; then
     echo "Not enough args ! "
     echo "Mandatory: please give INPUT_PATH RAWDATA_FILENAME"
     echo "as in the example below :"
-    echo "./run_decoding_workflow.sh /home/flp/data/h1/analog data-d0-2021_03_04__17_26_26__-20490-0.raw"
+    echo "./run_decoding_workflow.sh /home/flp/data/h1/analog data-d0-2021_03_04__17_26_26__-20490-0"
     exit 1
 fi
 
@@ -19,25 +19,40 @@ touch $CFGFILE
 echo "[input-MFT-0]">>$CFGFILE
 echo "dataOrigin = MFT" >>$CFGFILE
 echo "dataDescription = RAWDATA" >>$CFGFILE
-echo "filePath = "${INPUT_PATH}"/"${RAWDATA_FILENAME} >> $CFGFILE
+echo "filePath = "${INPUT_PATH}"/"${RAWDATA_FILENAME}".raw" >> $CFGFILE
 echo "Creation of "${CFGFILE}" : Done! "
 echo "-----------------------------"
 cat $CFGFILE
 echo "-----------------------------"
-echo "Run decoder RDHv6 on : "${INPUT_PATH}"/"${RAWDATA_FILENAME}
-timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+echo "Run decoder RDHv6 on : "${INPUT_PATH}"/"${RAWDATA_FILENAME}".raw"
+timestamp=$(date +"%Y_%m_%d_%H_%M_%S")
 mkdir -pv ${INPUT_PATH}/log/
 mkdir -pv ${INPUT_PATH}/digits/
-log=${INPUT_PATH}/log/decoder_$timestamp.out
-ROOTFILEPATH=${INPUT_PATH}/digits/
-#o2-raw-file-reader-workflow -b --delay 0.1 --nocheck-missing-stop --nocheck-starts-with-tf --nocheck-packet-increment --nocheck-hbf-jump --nocheck-hbf-per-tf --detect-tf0 --configKeyValues 'HBFUtils.nHBFPerTF=128' --input-conf $CFGFILE | o2-itsmft-stf-decoder-workflow -b --runmft --digits --writeHW --no-clusters --no-cluster-patterns --decoder-verbosity 1 | o2-itsmft-digit-writer-workflow -b --runmft --writeHW --disable-mc --outfile $ROOTFILEPATH$1.root > $log
-o2-raw-file-reader-workflow -b --delay 0.1 --nocheck-missing-stop --nocheck-starts-with-tf --nocheck-packet-increment --nocheck-hbf-jump --nocheck-hbf-per-tf --detect-tf0 --input-conf $CFGFILE | o2-itsmft-stf-decoder-workflow -b --runmft --digits --writeHW --no-clusters --no-cluster-patterns | o2-itsmft-digit-writer-workflow -b --runmft --writeHW --disable-mc --outfile $ROOTFILEPATH.root > $log
+log=${INPUT_PATH}/log/decoder-${RAWDATA_FILENAME}-$timestamp.out
+ROOTFILE=${INPUT_PATH}/digits/${RAWDATA_FILENAME}.root
+#o2-raw-file-reader-workflow -b --delay 0.1 --nocheck-missing-stop --nocheck-starts-with-tf --nocheck-packet-increment --nocheck-hbf-jump --nocheck-hbf-per-tf --detect-tf0 --configKeyValues 'HBFUtils.nHBFPerTF=128' --input-conf $CFGFILE | o2-itsmft-stf-decoder-workflow -b --runmft --digits --writeHW --no-clusters --no-cluster-patterns --decoder-verbosity 1 | o2-itsmft-digit-writer-workflow -b --runmft --writeHW --disable-mc --outfile $ROOTFILE.root > $log
+o2-raw-file-reader-workflow -b --delay 0.1 --nocheck-missing-stop --nocheck-starts-with-tf --nocheck-packet-increment --nocheck-hbf-jump --nocheck-hbf-per-tf --detect-tf0 --input-conf $CFGFILE | o2-itsmft-stf-decoder-workflow -b --runmft --digits --writeHW --no-clusters --no-cluster-patterns | o2-itsmft-digit-writer-workflow -b --runmft --writeHW --disable-mc --outfile $ROOTFILE > $log
 rm -f $CFGFILE
+
 echo "Log in : " $log
-echo "Digits in : " $ROOTFILEPATH
+echo "Digits in : " $ROOTFILE
 echo "Error Jump in RDH_packetCounter :  " 
 grep -r "Jump in RDH_packetCounter" $log | wc -l
+
 echo "[ERROR]"
 grep -r "[ERROR]" $log | wc -l
+
+echo "[ERROR] Failed to open input file"
+grep -r "[ERROR] Failed to open input file" $log | wc -l
+
+echo "[ERROR] Mismatch between flagged and calculated new TF start"
+grep -r "[ERROR] Mismatch between flagged and calculated new TF start" $log | wc -l
+
+echo "[ERROR] Unknown word"
+grep -r "[ERROR] Unknown word" $log | wc -l
+
+echo "[ERROR] SEVERE" 
+grep -r "[ERROR] SEVERE" $log | wc -l
+
 echo "Done !"
 echo "-----------------------------"
