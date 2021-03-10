74abd51827bf6682273d148ec192bf31ff10cb1c
rsadek <arakotoz@cern.ch>
2021-03-10 22:17:43 +0100

    add comments to describe the script and its arguments

--- run_decoding_workflow.sh
+++ run_decoding_workflow.sh
@@ -1,17 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Run this script to start the following workflow:
+#   o2-raw-file-reader-workflow | o2-itsmft-stf-decoder-workflow | o2-itsmft-digit-writer-workflow
+# The script takes 3 arguments:
+# - the delay (float, in s) needed to delay o2-raw-file-reader-workflow
+#   between to packets sent to the other DPLs (because they are too slow
+#   w.r.t. the raw file reader)
+# - the absolute path of the input raw data file to be decoded
+# - the raw data filename without the .raw at the end
+
 args=("$@")
-NUMBER_OF_ARGS=2
+NUMBER_OF_ARGS=3
 
 if [ $# -ne ${NUMBER_OF_ARGS} ]; then
     echo "Not enough args ! "
-    echo "Mandatory: please give INPUT_PATH RAWDATA_FILENAME"
+    echo "Mandatory: please give DELAY INPUT_PATH RAWDATA_FILENAME"
     echo "as in the example below :"
-    echo "./run_decoding_workflow.sh /home/flp/data/h1/analog data-d0-2021_03_04__17_26_26__-20490-0"
+    echo "./run_decoding_workflow.sh 0.1 /home/flp/data/h1/analog data-d0-2021_03_04__17_26_26__-20490-0"
     exit 1
 fi
 
-INPUT_PATH=${args[0]}
-RAWDATA_FILENAME=${args[1]}
+DELAY_S=${args[0]}
+INPUT_PATH=${args[1]}
+RAWDATA_FILENAME=${args[2]}
 
 
 CFGFILE=MFTraw.cfg
@@ -31,7 +42,7 @@ mkdir -pv ${INPUT_PATH}/digits/
 log=${INPUT_PATH}/log/decoder-${RAWDATA_FILENAME}-$timestamp.out
 ROOTFILE=${INPUT_PATH}/digits/${RAWDATA_FILENAME}.root
 #o2-raw-file-reader-workflow -b --delay 0.1 --nocheck-missing-stop --nocheck-starts-with-tf --nocheck-packet-increment --nocheck-hbf-jump --nocheck-hbf-per-tf --detect-tf0 --configKeyValues 'HBFUtils.nHBFPerTF=128' --input-conf $CFGFILE | o2-itsmft-stf-decoder-workflow -b --runmft --digits --writeHW --no-clusters --no-cluster-patterns --decoder-verbosity 1 | o2-itsmft-digit-writer-workflow -b --runmft --writeHW --disable-mc --outfile $ROOTFILE.root > $log
-o2-raw-file-reader-workflow -b --delay 0.1 --nocheck-missing-stop --nocheck-starts-with-tf --nocheck-packet-increment --nocheck-hbf-jump --nocheck-hbf-per-tf --detect-tf0 --input-conf $CFGFILE | o2-itsmft-stf-decoder-workflow -b --runmft --digits --writeHW --no-clusters --no-cluster-patterns | o2-itsmft-digit-writer-workflow -b --runmft --writeHW --disable-mc --outfile $ROOTFILE > $log
+o2-raw-file-reader-workflow -b --delay $DELAY_S --nocheck-missing-stop --nocheck-starts-with-tf --nocheck-packet-increment --nocheck-hbf-jump --nocheck-hbf-per-tf --detect-tf0 --input-conf $CFGFILE | o2-itsmft-stf-decoder-workflow -b --runmft --digits --writeHW --no-clusters --no-cluster-patterns | o2-itsmft-digit-writer-workflow -b --runmft --writeHW --disable-mc --outfile $ROOTFILE > $log
 rm -f $CFGFILE
 
 echo "Log in : " $log
