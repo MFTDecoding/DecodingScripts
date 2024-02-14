8075566588f342bec1193cc6d1332bf840e8bf75
mcoquet642 <74600025+mcoquet642@users.noreply.github.com>
2024-02-14 11:49:07 +0100

    Updating location of CCDB

--- NoiseScan/readCCDB.C
+++ NoiseScan/readCCDB.C
@@ -1,6 +1,6 @@
 void readCCDB(long timestamp=-1, float thresh=0){
 	o2::ccdb::CcdbApi api;
-	api.init("ccdb-test.cern.ch:8080");
+	api.init("http://alice-ccdb.cern.ch");
 	map<string, string> headers;
 	map<std::string, std::string> filter;
 	auto calib = api.retrieveFromTFileAny<o2::itsmft::NoiseMap>("MFT/Calib/NoiseMap/", filter , timestamp, &headers);
