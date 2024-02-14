f6716182a60e15019788c5509076c3f977e0c572
mcoquet642 <74600025+mcoquet642@users.noreply.github.com>
2024-02-14 11:49:51 +0100

    Update plotCCDB.C

--- NoiseScan/plotCCDB.C
+++ NoiseScan/plotCCDB.C
@@ -1,6 +1,6 @@
 void plotCCDB(){
 	o2::ccdb::CcdbApi api;
-	api.init("ccdb-test.cern.ch:8080");
+	api.init("http://alice-ccdb.cern.ch");
 	map<string, string> headers;
 	map<std::string, std::string> filter;
 	auto calib = api.retrieveFromTFileAny<o2::itsmft::NoiseMap>("MFT/Calib/NoiseMap/", filter , -1, &headers );
